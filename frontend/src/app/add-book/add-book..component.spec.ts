80d30f9f0c98777be79180fddfe187fec7c4f205
prabhat511 <prabhat.bhargav@informa.com>
2023-02-18 00:23:21 +0530

    feat: css hamberger menu animated and minor ui changes

--- frontend/src/app/add-book/add-book..component.spec.ts
+++ frontend/src/app/add-book/add-book..component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AddBookComponent } from './add-book..component';
 import { FormsModule } from '@angular/forms';
--- frontend/src/app/book-detail/book.component.spec.ts
+++ frontend/src/app/book-detail/book.component.spec.ts
@@ -1,25 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BooksService } from '../services/books.service';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { BookComponent } from './book.component';
 
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
+  let bookservice: BooksService;
+  let httpclient: HttpClient;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [BookComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    httpclient = TestBed.inject(HttpClient);
+    router = TestBed.inject(Router);
+    bookservice = new BooksService(httpclient);
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeDefined();
+    expect(component).toBeTruthy();
   });
+
+  describe('changeBook method', () => {
+    it('should navigate to update book component', () => {
+      jest.spyOn(router, 'navigate');
+      component.bookId = 'book1';
+      component.changeBook();
+      expect(router.navigate).toHaveBeenCalled();
+    })
+  })
+  describe('removeBook method', () => {
+    it('should delete the book by calling bookservice', () => {
+      const baseUrl = "https://bookstore-backend-hv3g.onrender.com/";
+      jest.spyOn(bookservice, 'deleteBook');
+      component.bookId = "book1";
+      component.removeBook();
+      // expect(bookservice.deleteBook).toHave(baseUrl+"book1");
+    })
+  })
+
+
+
 });
