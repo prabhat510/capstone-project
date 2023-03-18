4d3193012e57b658f99208809cf8807bf5d60673
prabhat511 <prabhat.bhargav@informa.com>
2023-03-18 13:48:48 +0530

    feat: ui changes in footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  year:number;
   constructor() { }
 
   ngOnInit(): void {
+    this.year = new Date().getFullYear();
   }
-
 }
