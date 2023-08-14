a5b8a58bdac7d336f4de9a2596921a519518a7fd
prabhat511 <prabhat.bhargav@informa.com>
2023-08-15 00:01:16 +0530

    feat: deploy new build

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -14,30 +14,35 @@ export class HomeComponent implements OnInit {
   filterTerm: string = '';
   loading = true;
   offset = 0;
-  limit = 10;
+  limit = 12;
   loadingMoreBooks = false;
-  constructor(private activatedroute: ActivatedRoute, private booksservice: BooksService, private router: Router) { }
+  pendingBooksCount = 0;
+  loadMoreOptionAvailable = true;
+  constructor(private booksservice: BooksService) { }
 
   ngOnInit() {
-    this.booksservice.getAllBooks(`https://bookstore-backend-hv3g.onrender.com/books?offset=${this.offset}&limit=${this.limit}`).subscribe((data) => {
-      this.books = this.books.concat(data);
-      this.loading = false;
-      this.offset += this.limit
-    });
-    this.activatedroute.url.subscribe(res => console.log("url is::::", res[0]));
+    this.loadBooks();
   }
   onSelect(event) {
     console.log(event.target.value);
     this.booksservice.getAllBooks(`https://bookstore-backend-hv3g.onrender.com/home/${event.target.value}`).subscribe(data => this.books = data
     )
   }
-  loadMoreBooks() {
-    this.loadingMoreBooks = true;
-    this.booksservice.getAllBooks(`https://bookstore-backend-hv3g.onrender.com/books?offset=${this.offset}&limit=${this.limit}`).subscribe((data) => {
-      this.books = this.books.concat(data);
-      this.loadingMoreBooks = false;
+  loadBooks() {
+    if(!this.loading){this.loadingMoreBooks = true;}
+    this.booksservice.getAllBooks(`https://bookstore-backend-hv3g.onrender.com/books?offset=${this.offset}&limit=${this.limit}`).subscribe((data: any) => {
+      this.books = this.books.concat(data.books);
       this.offset += this.limit
-    });
+      if(this.loading) {
+        this.pendingBooksCount = data.totalCount;
+        this.loading = false;
+      } else {
+        this.loadingMoreBooks = false;
+      }
+      this.pendingBooksCount -= this.books.length;
+      if(this.pendingBooksCount <=0)this.loadMoreOptionAvailable = false;
+      console.log('total count and pending count', data.totalCount, this.pendingBooksCount);
+    }); 
   }
   goToTop() {
     window.scrollTo({top: 0, behavior: 'smooth'});
