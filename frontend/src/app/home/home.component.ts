711a68e47b479c3038f678c1c34568527897427f
prabhat511 <prabhat.bhargav@informa.com>
2023-08-14 23:31:44 +0530

    feat: added load more and back to top

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -10,15 +10,19 @@ import { BooksService } from '../services/books.service';
 })
 export class HomeComponent implements OnInit {
 
-  books: any;
+  books: any = [];
   filterTerm: string = '';
   loading = true;
+  offset = 0;
+  limit = 10;
+  loadingMoreBooks = false;
   constructor(private activatedroute: ActivatedRoute, private booksservice: BooksService, private router: Router) { }
 
   ngOnInit() {
-    this.booksservice.getAllBooks('https://bookstore-backend-hv3g.onrender.com/home').subscribe((data) => {
-      this.books = data;
+    this.booksservice.getAllBooks(`https://bookstore-backend-hv3g.onrender.com/books?offset=${this.offset}&limit=${this.limit}`).subscribe((data) => {
+      this.books = this.books.concat(data);
       this.loading = false;
+      this.offset += this.limit
     });
     this.activatedroute.url.subscribe(res => console.log("url is::::", res[0]));
   }
@@ -26,5 +30,16 @@ export class HomeComponent implements OnInit {
     console.log(event.target.value);
     this.booksservice.getAllBooks(`https://bookstore-backend-hv3g.onrender.com/home/${event.target.value}`).subscribe(data => this.books = data
     )
+  }
+  loadMoreBooks() {
+    this.loadingMoreBooks = true;
+    this.booksservice.getAllBooks(`https://bookstore-backend-hv3g.onrender.com/books?offset=${this.offset}&limit=${this.limit}`).subscribe((data) => {
+      this.books = this.books.concat(data);
+      this.loadingMoreBooks = false;
+      this.offset += this.limit
+    });
+  }
+  goToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
   }
 }
