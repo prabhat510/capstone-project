acec29d3ff39fc03d435225a7d7e10a8696963db
prabhat511 <prabhat.bhargav@informa.com>
2023-11-18 22:22:21 +0530

    prerendering routes

--- frontend/src/app/book-detail/book.component.ts
+++ frontend/src/app/book-detail/book.component.ts
@@ -4,6 +4,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { getServiceUrl } from '../urls';
 import { AuthService } from '../services/auth.service';
 import { isPlatformBrowser } from '@angular/common';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-book',
@@ -11,33 +12,42 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
-  loading = true;
   isAdmin = false;
   bookId = '';
   isBrowser = false;
   book: any;
+  bookStateKey: any = makeStateKey<any>('book-data');
   constructor(private activatedroute: ActivatedRoute, private bookservice: BooksService, private router: Router,
     private authservice: AuthService,
-    @Inject(PLATFORM_ID) private platformId: Object) { 
-      this.isBrowser = isPlatformBrowser(platformId);
-    }
+    @Inject(PLATFORM_ID) private platformId: Object, private transferState: TransferState) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   ngOnInit(): void {
-    if(this.isBrowser) {
-      const isLoggedIn = this.authservice.isLoggedIn;
+    if (this.transferState.hasKey(this.bookStateKey)) {
+      console.log('key is present');
+      const cachedBookData = this.transferState.get(this.bookStateKey, {});
+      this.transferState.remove(this.bookStateKey);
+      this.book = cachedBookData;
+      console.log('cacheddata', cachedBookData);
+    } else if (!this.isBrowser) {
       this.bookId = this.activatedroute.snapshot.paramMap.get('id');
-      this.isAdmin = isLoggedIn && JSON.parse(localStorage.getItem('userData'))? JSON.parse(localStorage.getItem('userData')).isAdmin : false;
+      console.log('data not cached fetching it from the server');
       this.bookservice.getBook(`${getServiceUrl().bookServiceAPI}/books/${this.bookId}`).subscribe(data => {
         this.book = data;
-        this.loading = false;
-        this.bookservice.scrollToTop();
+        this.transferState.set(this.bookStateKey, data);
       });
     }
+    if (this.isBrowser) {
+      const isLoggedIn = this.authservice.isLoggedIn;
+      this.isAdmin = isLoggedIn && JSON.parse(localStorage.getItem('userData')) ? JSON.parse(localStorage.getItem('userData')).isAdmin : false;
+      this.bookservice.scrollToTop();
+    }
   }
-  
+
   removeBook() {
     this.bookservice.deleteBook(`${getServiceUrl().bookServiceAPI}/books/remove/${this.bookId}`).subscribe();
-    setTimeout(()=> {
+    setTimeout(() => {
       this.router.navigate(['']).then(() => window.location.reload());
     }, 1000)
   }
